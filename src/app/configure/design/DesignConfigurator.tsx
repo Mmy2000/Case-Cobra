95872c3fba113d40f5ea5c4cb4cfa81ab40be626
Mmy2000 <yahianada005@gmail.com>
2024-10-08 19:49:37 +0300

    creating an design page and add some functionality

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,4 +1,3 @@
-
 interface DesignConfiguratorProps {
   configId: string;
   imageUrl: string;
@@ -10,10 +9,14 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
-
-    return(
-        <div></div>
-    )
+  return (
+    <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
+      <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+        <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default DesignConfigurator
+export default DesignConfigurator;
