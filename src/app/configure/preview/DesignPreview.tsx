5915e8ef0331891133c4604980749ccc906efd26
Mmy2000 <yahianada005@gmail.com>
2024-10-09 02:04:28 +0300

    creating an preview page and add some functionality

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -12,8 +12,23 @@ import Confetti from "react-dom-confetti";
 
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
+    
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
   useEffect(() => setShowConfetti(true));
+  const { color, model, finish, material } = configuration;
+
+  const tw = COLORS.find(
+    (supportedColor) => supportedColor.value === color
+  )?.tw;
+
+  const { label: modelLabel } = MODELS.options.find(
+    ({ value }) => value === model
+  )!;
+   let totalPrice = BASE_PRICE;
+   if (material === "polycarbonate")
+     totalPrice += PRODUCT_PRICES.material.polycarbonate;
+   if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
+  
   return (
     <>
       <div
@@ -27,10 +42,39 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
       </div>
       <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
-          {/* <Phone
+          <Phone
             className={cn(`bg-${tw}`, "max-w-[150px] md:max-w-full")}
             imgSrc={configuration.croppedImageUrl!}
-          /> */}
+          />
+        </div>
+        <div className="mt-6 sm:col-span-9 md:row-end-1">
+          <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+            Your {modelLabel} Case
+          </h3>
+          <div className="mt-3 flex items-center gap-1.5 text-base">
+            <Check className="h-4 w-4 text-green-500" />
+            In stock and ready to ship
+          </div>
+        </div>
+        <div className="sm:col-span-12 md:col-span-9 text-base">
+          <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+            <div>
+              <p className="font-medium text-zinc-950">Highlights</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>Wireless charging compatible</li>
+                <li>TPU shock absorption</li>
+                <li>Packaging made from recycled materials</li>
+                <li>5 year print warranty</li>
+              </ol>
+            </div>
+            <div>
+              <p className="font-medium text-zinc-950">Materials</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>High-quality, durable material</li>
+                <li>Scratch- and fingerprint resistant coating</li>
+              </ol>
+            </div>
+          </div>
         </div>
       </div>
     </>
