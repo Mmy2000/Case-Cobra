20936e050e8a719fabcf7de45dc8e9a8bd3f080f
Mmy2000 <yahianada005@gmail.com>
2024-10-08 19:46:53 +0300

    creating an design page and add some functionality

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,19 @@
+
+interface DesignConfiguratorProps {
+  configId: string;
+  imageUrl: string;
+  imageDimensions: { width: number; height: number };
+}
+
+const DesignConfigurator = ({
+  configId,
+  imageUrl,
+  imageDimensions,
+}: DesignConfiguratorProps) => {
+
+    return(
+        <div></div>
+    )
+};
+
+export default DesignConfigurator
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
 
 interface PageProps {
   searchParams: {
@@ -10,9 +11,27 @@ interface PageProps {
 const Page = async ({ searchParams }: PageProps) => {
     const { id } = searchParams;
 
+    if (!id || typeof id !== "string") {
+      return notFound();
+    }
+
+    const configuration = await db.configuration.findUnique({
+      where: { id },
+    });
+
+    if (!configuration) {
+      return notFound();
+    }
+
+    const { imageUrl, width, height } = configuration;
+
     return (
-        <p>{id}</p>
-    )
+      <DesignConfigurator
+        configId={configuration.id}
+        imageDimensions={{ width, height }}
+        imageUrl={imageUrl}
+      />
+    );
 };
 
 export default Page;
