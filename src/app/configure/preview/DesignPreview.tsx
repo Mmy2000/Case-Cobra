3bf01c87b4803da149f2e77f040cbd15c284871b
Mmy2000 <yahianada005@gmail.com>
2024-10-09 13:16:29 +0300

    starting with payment

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -31,6 +31,11 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
     totalPrice += PRODUCT_PRICES.material.polycarbonate;
   if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
 
+  const {} = useMutation({
+    mutationKey: ["get-checkout-session"],
+    // mutationFn: createCheckoutSession,
+  });
+
   return (
     <>
       <div
